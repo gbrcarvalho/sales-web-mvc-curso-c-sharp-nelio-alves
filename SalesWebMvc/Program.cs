11eec31644ae95526c8810b7d5fb6f3381a8c48c
Gabriel Silva Carvalho <gccomputacao@gmail.com>
2025-09-26 00:52:03 -0300

    add: seeding service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration; 
 using Microsoft.Extensions.DependencyInjection;
-using SalesWebMvc.Models;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.");
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<SalesWebMvcContext>(
     options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString),
     builder => builder.MigrationsAssembly("SalesWebMvc")
  ));
+
+builder.Services.AddScoped<SeedingService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -24,7 +26,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    Console.WriteLine("Development!");
 }
+SeedingService seedingService = app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>();
+seedingService.Seed();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
