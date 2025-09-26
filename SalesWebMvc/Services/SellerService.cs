e37b6c4c12aab1ba9390c9065783de5c5e525fc3
Gabriel Silva Carvalho <gccomputacao@gmail.com>
2025-09-26 16:21:13 -0300

    add: seller details

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
 ﻿using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -17,7 +18,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
