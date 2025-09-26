9e768e93f484ce4ed932dfe7ac31f44f4e74fcd1
Gabriel Silva Carvalho <gccomputacao@gmail.com>
2025-09-26 15:00:11 -0300

    add: seller form view model and department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(
 
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Services
         public void Insert(Seller seller)
         {
             //seller.Department = new Department(5, "Food");
-            seller.Department = _context.Department.First();
+            //seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
